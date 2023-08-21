8e9ba48fb8a2352b6b9b82e3d8ccfa2c98aa1f8a
Ricardo Gimenez <cpd2@santanna.g12.br>
2023-08-21 10:53:57 -0300

    criando páginas para testar guard

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -7,7 +7,9 @@
       <!-- specify the link by passing the `to` prop. -->
       <!-- `<router-link>` will render an `<a>` tag with the correct `href` attribute -->
       <router-link to="/">Go to Home</router-link>
+      <router-link to="/dashboard">Dashboard</router-link>
       <router-link to="/about">Go to About</router-link>
+      <router-link to="/login">Login</router-link>
     </p>
     <router-view></router-view>
   </div>
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,10 +2,14 @@ import { createRouter, createWebHashHistory } from 'vue-router';
 
 import Home from './pages/Home.vue';
 import About from './pages/About.vue';
+import Dashboard from './pages/Dashboard.vue';
+import Login from './pages/login.vue';
 
 const routes = [
   { path: '/', component: Home },
   { path: '/about', component: About },
+  { path: '/login', component: Login },
+  { path: '/dashboard', component: Dashboard, meta: { requiresAuth: true } },
 ];
 
 export const router = createRouter({
